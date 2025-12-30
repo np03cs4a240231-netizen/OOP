aaf6dc1ee836d6a1e66bafb6bf476ae04e13d161
Pranjal-Khanal.github.io <np03cs4a240231@heraldcollege.edu.np>
2025-12-30 09:52:51 +0545

    Added Student class with constructor

---
+++ Student.java
@@ -0,0 +1,12 @@
+package Workshop7;
+
+public class Student {
+    private String name;
+    private int age;
+
+    // Constructor
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
